91193b2807444a85eb017d41618c1b89b1789ee4
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-06-01 13:36:52 -0400

    Extract AST operations

---
+++ src/refactorings/ast.ts
@@ -0,0 +1,40 @@
+import { parse } from "@babel/parser";
+import traverse, { TraverseOptions } from "@babel/traverse";
+import * as t from "@babel/types";
+
+import { Code } from "./i-write-updates";
+
+export { traverseAST, isStringLiteral };
+
+function traverseAST(code: Code, opts: TraverseOptions): void {
+  const ast = parse(code, {
+    // Parse in strict mode and allow module declarations
+    sourceType: "module",
+
+    plugins: [
+      "classProperties",
+      "classPrivateProperties",
+      "classPrivateMethods",
+      "jsx",
+      "typescript"
+    ]
+  });
+
+  traverse(ast, opts);
+}
+
+function isStringLiteral(
+  node: object | null | undefined,
+  opts?: object | null
+): node is StringLiteral {
+  return t.isStringLiteral(node, opts);
+}
+
+interface StringLiteral extends t.StringLiteral {
+  extra: Extra;
+}
+
+interface Extra {
+  raw: string;
+  rawValue: string;
+}
--- src/refactorings/extract-variable.ts
+++ src/refactorings/extract-variable.ts
@@ -1,30 +1,11 @@
-import { parse } from "@babel/parser";
-import traverse from "@babel/traverse";
-import * as t from "@babel/types";
-
 import { Code, WriteUpdates } from "./i-write-updates";
 import { DelegateToEditor } from "./i-delegate-to-editor";
 import { renameSymbol } from "./rename-symbol";
 import { Selection } from "./selection";
 import { Position } from "./position";
+import { traverseAST, isStringLiteral } from "./ast";
 
 export { extractVariable };
-
-interface StringLiteral extends t.StringLiteral {
-  extra: Extra;
-}
-
-interface Extra {
-  raw: string;
-  rawValue: string;
-}
-
-function isStringLiteral(
-  node: object | null | undefined,
-  opts?: object | null
-): node is StringLiteral {
-  return t.isStringLiteral(node, opts);
-}
 
 async function extractVariable(
   code: Code,
@@ -32,22 +13,9 @@ async function extractVariable(
   writeUpdates: WriteUpdates,
   delegateToEditor: DelegateToEditor
 ) {
-  const ast = parse(code, {
-    // Parse in strict mode and allow module declarations
-    sourceType: "module",
-
-    plugins: [
-      "classProperties",
-      "classPrivateProperties",
-      "classPrivateMethods",
-      "jsx",
-      "typescript"
-    ]
-  });
-
   let extractedCode;
 
-  traverse(ast, {
+  traverseAST(code, {
     enter(path) {
       if (isStringLiteral(path.node) && path.node.loc) {
         if (selection.start.isEqualTo(Position.fromAST(path.node.loc.start))) {
