bbbe40108a0ad10d47469e3dd72a36b750a0c4c7
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-09-09 09:57:38 -0400

    Handle all operators

--- src/ast/discriminant.test.ts
+++ src/ast/discriminant.test.ts
@@ -1,6 +1,6 @@
 import * as t from "@babel/types";
 
-import { getDiscriminantFrom } from "./discriminant";
+import { getDiscriminantFrom, VALID_OPERATORS } from "./discriminant";
 
 describe("AST Discriminant", () => {
   it("should return null if given expression is null", () => {
@@ -11,30 +11,118 @@ describe("AST Discriminant", () => {
     expect(discriminant).toBe(null);
   });
 
-  it("should return the identifier of given binary expression when it's on the left", () => {
-    const name = t.identifier("name");
-    const expression = t.binaryExpression("===", name, t.stringLiteral("John"));
+  describe("BinaryExpression", () => {
+    VALID_OPERATORS.forEach(operator =>
+      shouldIdentifyDiscriminantWith(operator)
+    );
 
-    const discriminant = getDiscriminantFrom(expression);
+    const INVALID_OPERATORS: t.BinaryExpression["operator"][] = [
+      "+",
+      "-",
+      "/",
+      "%",
+      "*",
+      "**",
+      "&",
+      "|",
+      ">>",
+      ">>>",
+      "<<",
+      "^",
+      "!=",
+      "!==",
+      "in",
+      "instanceof",
+      ">",
+      "<",
+      ">=",
+      "<="
+    ];
+    INVALID_OPERATORS.forEach(operator => shouldReturnNullWith(operator));
 
-    expect(discriminant).toBe(name);
-  });
+    function shouldIdentifyDiscriminantWith(
+      operator: t.BinaryExpression["operator"]
+    ) {
+      describe(`Operator "${operator}"`, () => {
+        it("should return the identifier when it's on the left", () => {
+          const name = t.identifier("name");
+          const expression = t.binaryExpression(
+            operator,
+            name,
+            t.stringLiteral("John")
+          );
 
-  it("should return the identifier of given binary expression when it's on the right", () => {
-    const name = t.identifier("name");
-    const expression = t.binaryExpression("===", t.stringLiteral("John"), name);
+          const discriminant = getDiscriminantFrom(expression);
 
-    const discriminant = getDiscriminantFrom(expression);
+          expect(discriminant).toBe(name);
+        });
 
-    expect(discriminant).toBe(name);
-  });
+        it("should return the identifier when it's on the right", () => {
+          const name = t.identifier("name");
+          const expression = t.binaryExpression(
+            operator,
+            t.stringLiteral("John"),
+            name
+          );
 
-  it("should return the left identifier of given binary expression when both nodes are identifiers", () => {
-    const john = t.identifier("john");
-    const expression = t.binaryExpression("===", john, t.identifier("name"));
+          const discriminant = getDiscriminantFrom(expression);
 
-    const discriminant = getDiscriminantFrom(expression);
+          expect(discriminant).toBe(name);
+        });
 
-    expect(discriminant).toBe(john);
+        it("should return the left identifier when both nodes are identifiers", () => {
+          const john = t.identifier("john");
+          const expression = t.binaryExpression(
+            operator,
+            john,
+            t.identifier("name")
+          );
+
+          const discriminant = getDiscriminantFrom(expression);
+
+          expect(discriminant).toBe(john);
+        });
+      });
+    }
+
+    function shouldReturnNullWith(operator: t.BinaryExpression["operator"]) {
+      describe(`Operator "${operator}"`, () => {
+        it("should return null when identifier is on the left", () => {
+          const expression = t.binaryExpression(
+            operator,
+            t.identifier("name"),
+            t.stringLiteral("John")
+          );
+
+          const discriminant = getDiscriminantFrom(expression);
+
+          expect(discriminant).toBe(null);
+        });
+
+        it("should return null when identifier is on the right", () => {
+          const expression = t.binaryExpression(
+            operator,
+            t.stringLiteral("John"),
+            t.identifier("name")
+          );
+
+          const discriminant = getDiscriminantFrom(expression);
+
+          expect(discriminant).toBe(null);
+        });
+
+        it("should return null when both nodes are identifiers", () => {
+          const expression = t.binaryExpression(
+            operator,
+            t.identifier("john"),
+            t.identifier("name")
+          );
+
+          const discriminant = getDiscriminantFrom(expression);
+
+          expect(discriminant).toBe(null);
+        });
+      });
+    }
   });
 });
--- src/ast/discriminant.ts
+++ src/ast/discriminant.ts
@@ -1,11 +1,12 @@
 import * as t from "@babel/types";
 
-export { getDiscriminantFrom };
+export { getDiscriminantFrom, VALID_OPERATORS };
+
+const VALID_OPERATORS: t.BinaryExpression["operator"][] = ["==", "==="];
 
 function getDiscriminantFrom(expression: t.Expression): t.Expression | null {
-  if (t.isBinaryExpression(expression)) {
-    return t.isIdentifier(expression.left) ? expression.left : expression.right;
-  }
+  if (!t.isBinaryExpression(expression)) return null;
+  if (!VALID_OPERATORS.includes(expression.operator)) return null;
 
-  return null;
+  return t.isIdentifier(expression.left) ? expression.left : expression.right;
 }
