7c1565c16b1b0e000fd2e1634120727973c99ea0
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-09-14 15:53:55 -0400

    Extract destructured object pattern tests

---
+++ src/refactorings/inline-variable-or-function/inline-variable.object-pattern.test.ts
@@ -0,0 +1,90 @@
+import { Code } from "../../editor/editor";
+import { Selection } from "../../editor/selection";
+import { InMemoryEditor } from "../../editor/adapters/in-memory-editor";
+import { testEach } from "../../tests-helpers";
+
+import { inlineVariable } from "./inline-variable";
+
+describe("Inline Variable - Object Pattern", () => {
+  testEach<{ code: Code; selection: Selection; expected: Code }>(
+    "should inline the destructured variable value",
+    [
+      {
+        description: "basic scenario",
+        code: `const { userId } = session;
+messages.map(message => ({ userId }));`,
+        selection: Selection.cursorAt(0, 9),
+        expected: `messages.map(message => ({ userId: session.userId }));`
+      },
+      {
+        description: "renamed, cursor on key",
+        code: `const { userId: id } = session;
+messages.map(message => ({ id }));`,
+        selection: Selection.cursorAt(0, 9),
+        expected: `messages.map(message => ({ id: session.userId }));`
+      },
+      {
+        description: "renamed, cursor on value",
+        code: `const { userId: id } = session;
+messages.map(message => ({ id }));`,
+        selection: Selection.cursorAt(0, 17),
+        expected: `messages.map(message => ({ id: session.userId }));`
+      },
+      {
+        description: "not assigned to another object",
+        code: `const { userId } = session;
+console.log(userId);`,
+        selection: Selection.cursorAt(0, 9),
+        expected: `console.log(session.userId);`
+      },
+      {
+        description: "init being a member expression",
+        code: `const { id } = session.user;
+console.log(id);`,
+        selection: Selection.cursorAt(0, 9),
+        expected: `console.log(session.user.id);`
+      },
+      {
+        description: "init being a member expression with a numeric literal",
+        code: `const { id } = session.users[0];
+console.log(id);`,
+        selection: Selection.cursorAt(0, 9),
+        expected: `console.log(session.users[0].id);`
+      },
+      {
+        description: "init being a member expression with a string literal",
+        code: `const { id } = session.users["first"];
+console.log(id);`,
+        selection: Selection.cursorAt(0, 9),
+        expected: `console.log(session.users["first"].id);`
+      },
+      {
+        description: "nested",
+        code: `const { user: { id } } = session;
+console.log(id);`,
+        selection: Selection.cursorAt(0, 17),
+        expected: `console.log(session.user.id);`
+      },
+      {
+        description: "nested, init being a member expression",
+        code: `const { user: { data: { n: firstName} } } = session.data[0];
+console.log(firstName);`,
+        selection: Selection.cursorAt(0, 28),
+        expected: `console.log(session.data[0].user.data.n);`
+      }
+    ],
+    async ({ code, selection, expected }) => {
+      const result = await doInlineVariable(code, selection);
+      expect(result).toBe(expected);
+    }
+  );
+
+  async function doInlineVariable(
+    code: Code,
+    selection: Selection
+  ): Promise<Code> {
+    const editor = new InMemoryEditor(code);
+    await inlineVariable(code, selection, editor);
+    return editor.code;
+  }
+});
