d8ed502e76223313e969a706e4f85b0b093e237b
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-09-21 08:17:02 -0400

    Create new refactoring

---
+++ src/refactorings/remove-dead-code/remove-dead-code.test.ts
@@ -0,0 +1,45 @@
+import { Editor, ErrorReason, Code } from "../../editor/editor";
+import { Selection } from "../../editor/selection";
+import { InMemoryEditor } from "../../editor/adapters/in-memory-editor";
+import { testEach } from "../../tests-helpers";
+
+import { removeDeadCode } from "./remove-dead-code";
+
+describe("Remove Dead Code", () => {
+  let showErrorMessage: Editor["showError"];
+
+  beforeEach(() => {
+    showErrorMessage = jest.fn();
+  });
+
+  testEach<{ code: Code; selection?: Selection; expected: Code }>(
+    "should remove dead code",
+    [
+      // TODO: write successful test cases here
+    ],
+    async ({ code, selection = Selection.cursorAt(0, 0), expected }) => {
+      const result = await doRemoveDeadCode(code, selection);
+
+      expect(result).toBe(expected);
+    }
+  );
+
+  it("should show an error message if refactoring can't be made", async () => {
+    const code = `// This is a comment, can't be refactored`;
+    const selection = Selection.cursorAt(0, 0);
+
+    await doRemoveDeadCode(code, selection);
+
+    expect(showErrorMessage).toBeCalledWith(ErrorReason.DidNotFoundDeadCode);
+  });
+
+  async function doRemoveDeadCode(
+    code: Code,
+    selection: Selection
+  ): Promise<Code> {
+    const editor = new InMemoryEditor(code);
+    editor.showError = showErrorMessage;
+    await removeDeadCode(code, selection, editor);
+    return editor.code;
+  }
+});
---
+++ src/refactorings/remove-dead-code/remove-dead-code.ts
@@ -0,0 +1,30 @@
+import { Editor, Code, ErrorReason } from "../../editor/editor";
+import { Selection } from "../../editor/selection";
+import * as ast from "../../ast";
+
+export { removeDeadCode, hasDeadCode };
+
+async function removeDeadCode(
+  code: Code,
+  selection: Selection,
+  editor: Editor
+) {
+  const updatedCode = updateCode(code, selection);
+
+  if (!updatedCode.hasCodeChanged) {
+    editor.showError(ErrorReason.DidNotFoundDeadCode);
+    return;
+  }
+
+  await editor.write(updatedCode.code);
+}
+
+function hasDeadCode(code: Code, selection: Selection): boolean {
+  return updateCode(code, selection).hasCodeChanged;
+}
+
+function updateCode(code: Code, selection: Selection): ast.Transformed {
+  return ast.transform(code, {
+    // TODO: implement the transformation here 🧙‍
+  });
+}
