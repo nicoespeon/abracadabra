9a231892aad7d38e5d6af7bf2a515c1d5ac9c1db
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-28 22:57:10 -0400

    Refactor ConvertIfElseToTernaryActionProvider

--- src/refactorings/convert-if-else-to-ternary/action-provider.ts
+++ src/refactorings/convert-if-else-to-ternary/action-provider.ts
@@ -1,38 +1,16 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { hasIfElseToConvert } from "./convert-if-else-to-ternary";
 
-class ConvertIfElseToTernaryActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!hasIfElseToConvert(code, selection)) return;
-
-    const action = new vscode.CodeAction(
-      "✨ Convert if/else to ternary",
-      this.kind
-    );
-    action.isPreferred = false;
-    action.command = {
-      command: commandKey,
-      title: "Convert If/Else to Ternary"
-    };
-
-    return [action];
-  }
+class ConvertIfElseToTernaryActionProvider extends RefactoringActionProvider {
+  actionMessage = "Convert if/else to ternary";
+  commandKey = commandKey;
+  title = "Convert If/Else to Ternary";
+  canPerformRefactoring = hasIfElseToConvert;
 }
 
 export default createActionProviderFor(
