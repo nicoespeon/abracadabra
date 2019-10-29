cf627f065044544d0cb9d82ba294bcfa67a53abf
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-29 18:50:03 -0400

    Refactor RemoveBracesFromArrowFunctionActionProvider

--- src/refactorings/remove-braces-from-arrow-function/action-provider.ts
+++ src/refactorings/remove-braces-from-arrow-function/action-provider.ts
@@ -1,39 +1,16 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { hasBracesToRemoveFromArrowFunction } from "./remove-braces-from-arrow-function";
 
-class RemoveBracesFromArrowFunctionActionProvider
-  implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!hasBracesToRemoveFromArrowFunction(code, selection)) return;
-
-    const action = new vscode.CodeAction(
-      "✨ Remove braces from arrow function",
-      this.kind
-    );
-    action.isPreferred = false;
-    action.command = {
-      command: commandKey,
-      title: "Remove Braces from Arrow Function"
-    };
-
-    return [action];
-  }
+class RemoveBracesFromArrowFunctionActionProvider extends RefactoringActionProvider {
+  actionMessage = "Remove braces from arrow function";
+  commandKey = commandKey;
+  title = "Remove Braces from Arrow Function";
+  canPerformRefactoring = hasBracesToRemoveFromArrowFunction;
 }
 
 export default createActionProviderFor(
