74ad1d7d989fefec7bfc0dd28cc2d46682b6e5ee
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-29 18:51:00 -0400

    Refactor RemoveDeadCodeActionProvider

--- src/refactorings/remove-dead-code/action-provider.ts
+++ src/refactorings/remove-dead-code/action-provider.ts
@@ -1,35 +1,17 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { hasDeadCode } from "./remove-dead-code";
 
-class RemoveDeadCodeActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!hasDeadCode(code, selection)) return;
-
-    const action = new vscode.CodeAction("✨ Remove dead code", this.kind);
-    action.isPreferred = true;
-    action.command = {
-      command: commandKey,
-      title: "Remove Dead Code"
-    };
-
-    return [action];
-  }
+class RemoveDeadCodeActionProvider extends RefactoringActionProvider {
+  actionMessage = "Remove dead code";
+  commandKey = commandKey;
+  title = "Remove Dead Code";
+  canPerformRefactoring = hasDeadCode;
+  isPreferred = true;
 }
 
 export default createActionProviderFor(new RemoveDeadCodeActionProvider());
