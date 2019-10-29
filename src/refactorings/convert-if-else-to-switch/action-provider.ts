3fddabdc14dcac55a5fca23dad3f959c8fdb9634
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-28 22:54:21 -0400

    Refactor ConvertIfElseToSwitchActionProvider

--- src/refactorings/convert-if-else-to-switch/action-provider.ts
+++ src/refactorings/convert-if-else-to-switch/action-provider.ts
@@ -1,38 +1,17 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { hasIfElseToConvert } from "./convert-if-else-to-switch";
 
-class ConvertIfElseToSwitchActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!hasIfElseToConvert(code, selection)) return;
-
-    const action = new vscode.CodeAction(
-      "✨ Convert if/else to switch",
-      this.kind
-    );
-    action.isPreferred = true;
-    action.command = {
-      command: commandKey,
-      title: "Convert If/Else to Switch"
-    };
-
-    return [action];
-  }
+class ConvertIfElseToSwitchActionProvider extends RefactoringActionProvider {
+  actionMessage = "Convert if/else to switch";
+  commandKey = commandKey;
+  title = "Convert If/Else to Switch";
+  canPerformRefactoring = hasIfElseToConvert;
+  isPreferred = true;
 }
 
 export default createActionProviderFor(
