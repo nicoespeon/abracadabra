54b670d462dbe0db66097fe764830d0fb65b6a4b
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-29 18:53:42 -0400

    Refactor ReplaceBinaryWithAssignmentActionProvider

--- src/refactorings/replace-binary-with-assignment/action-provider.ts
+++ src/refactorings/replace-binary-with-assignment/action-provider.ts
@@ -1,38 +1,23 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { tryToReplaceBinaryWithAssignment } from "./replace-binary-with-assignment";
+import { Code } from "../../editor/editor";
+import { Selection } from "../../editor/selection";
 
-class ReplaceBinaryWithAssignmentActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
+class ReplaceBinaryWithAssignmentActionProvider extends RefactoringActionProvider {
+  commandKey = commandKey;
+  title = "Replace Binary With Assignment";
+  isPreferred = true;
 
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
+  canPerformRefactoring(code: Code, selection: Selection) {
     const attempt = tryToReplaceBinaryWithAssignment(code, selection);
-    if (!attempt.canReplace) return;
+    this.actionMessage = `Replace = with ${attempt.operator}=`;
 
-    const action = new vscode.CodeAction(
-      `✨ Replace = with ${attempt.operator}=`,
-      this.kind
-    );
-    action.isPreferred = true;
-    action.command = {
-      command: commandKey,
-      title: "Replace Binary With Assignment"
-    };
-
-    return [action];
+    return attempt.canReplace;
   }
 }
 
