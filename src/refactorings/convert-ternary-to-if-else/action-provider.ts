1fbf77e8b60eafe08d43ef09d34827a04c444acc
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-28 22:59:36 -0400

    Refactor ConvertToTemplateLiteralActionProvider

--- src/refactorings/convert-ternary-to-if-else/action-provider.ts
+++ src/refactorings/convert-ternary-to-if-else/action-provider.ts
@@ -1,38 +1,16 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { hasTernaryToConvert } from "./convert-ternary-to-if-else";
 
-class ConvertTernaryToIfElseActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!hasTernaryToConvert(code, selection)) return;
-
-    const action = new vscode.CodeAction(
-      "✨ Convert ternary to if/else",
-      this.kind
-    );
-    action.isPreferred = false;
-    action.command = {
-      command: commandKey,
-      title: "Convert Ternary to If/Else"
-    };
-
-    return [action];
-  }
+class ConvertTernaryToIfElseActionProvider extends RefactoringActionProvider {
+  actionMessage = "Convert ternary to if/else";
+  commandKey = commandKey;
+  title = "Convert Ternary To If/Else";
+  canPerformRefactoring = hasTernaryToConvert;
 }
 
 export default createActionProviderFor(
--- src/refactorings/convert-to-template-literal/action-provider.ts
+++ src/refactorings/convert-to-template-literal/action-provider.ts
@@ -1,38 +1,17 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { canConvertToTemplateLiteral } from "./convert-to-template-literal";
 
-class ConvertToTemplateLiteralActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!canConvertToTemplateLiteral(code, selection)) return;
-
-    const action = new vscode.CodeAction(
-      "✨ Convert to template literal",
-      this.kind
-    );
-    action.isPreferred = true;
-    action.command = {
-      command: commandKey,
-      title: "Convert to Template Literal"
-    };
-
-    return [action];
-  }
+class ConvertToTemplateLiteralActionProvider extends RefactoringActionProvider {
+  actionMessage = "Convert to template literal";
+  commandKey = commandKey;
+  title = "Convert to Template Literal";
+  canPerformRefactoring = canConvertToTemplateLiteral;
+  isPreferred = true;
 }
 
 export default createActionProviderFor(
