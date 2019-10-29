0cfbc2f1411017cb50c6e1c7b86b44efb4e5c026
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-10-29 18:41:45 -0400

    Refactor MergeWithPreviousIfStatementActionProvider

--- src/refactorings/merge-with-previous-if-statement/action-provider.ts
+++ src/refactorings/merge-with-previous-if-statement/action-provider.ts
@@ -1,38 +1,17 @@
-import * as vscode from "vscode";
-
 import {
-  CodeActionProvider,
-  createActionProviderFor
+  createActionProviderFor,
+  RefactoringActionProvider
 } from "../../action-providers";
-import { createSelectionFromVSCode } from "../../editor/adapters/vscode-editor";
 
 import { commandKey } from "./command";
 import { canMergeWithPreviousIf } from "./merge-with-previous-if-statement";
 
-class MergeWithPreviousIfStatementActionProvider implements CodeActionProvider {
-  public readonly kind = vscode.CodeActionKind.RefactorRewrite;
-
-  public provideCodeActions(
-    document: vscode.TextDocument,
-    range: vscode.Range | vscode.Selection
-  ): vscode.ProviderResult<vscode.CodeAction[]> {
-    const code = document.getText();
-    const selection = createSelectionFromVSCode(range);
-
-    if (!canMergeWithPreviousIf(code, selection)) return;
-
-    const action = new vscode.CodeAction(
-      "✨ Merge with previous if",
-      this.kind
-    );
-    action.isPreferred = true;
-    action.command = {
-      command: commandKey,
-      title: "Merge With Previous If Statement"
-    };
-
-    return [action];
-  }
+class MergeWithPreviousIfStatementActionProvider extends RefactoringActionProvider {
+  actionMessage = "Merge with previous if";
+  commandKey = commandKey;
+  title = "Merge With Previous If Statement";
+  canPerformRefactoring = canMergeWithPreviousIf;
+  isPreferred = true;
 }
 
 export default createActionProviderFor(
