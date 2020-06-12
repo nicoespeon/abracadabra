836d6fb7b23c6c716651d8576b2551d35f72e241
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-06-12 00:34:54 -0400

    Correct label

--- src/refactorings/extract/extract-variable/parts.test.ts
+++ src/refactorings/extract/extract-variable/parts.test.ts
@@ -4,7 +4,7 @@ import { Selection } from "../../../editor/selection";
 import { Position } from "../../../editor/position";
 
 describe("Parts", () => {
-  it("should identify different parts of a string", () => {
+  it("should identify different parts of a Code", () => {
     const code = "Hello world!";
     const selection = new Selection([0, 6], [0, 11]);
     const offset = new Position(0, 0);
