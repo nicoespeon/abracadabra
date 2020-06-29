d816dd38dfcb126e319431ffaacf17596e4d9ddd
Oliver Joseph Ash <oliverjash@gmail.com>
2020-06-29 14:20:03 +0100

    Initial commit

---
+++ src/refactorings/convert-function-declaration-to-arrow-function/convert-function-declaration-to-arrow-function.ts
@@ -0,0 +1,61 @@
+import { Editor, Code, ErrorReason } from "../../editor/editor";
+import { Selection } from "../../editor/selection";
+import * as t from "../../ast";
+
+export { convertFunctionDeclarationToArrowFunction, createVisitor };
+
+async function convertFunctionDeclarationToArrowFunction(
+  code: Code,
+  selection: Selection,
+  editor: Editor
+) {
+  const updatedCode = updateCode(t.parse(code), selection);
+
+  if (!updatedCode.hasCodeChanged) {
+    editor.showError(ErrorReason.DidNotFindFunctionDeclarationToConvert);
+    return;
+  }
+
+  await editor.write(updatedCode.code);
+}
+
+function updateCode(ast: t.AST, selection: Selection): t.Transformed {
+  return t.transformAST(
+    ast,
+    createVisitor(selection, path => {
+      const { node } = path;
+      const name = node.id ? node.id.name : "converted";
+      const identifier = t.identifier(name);
+
+      const arrowFunctionExpression = t.arrowFunctionExpression(
+        node.params,
+        node.body,
+        node.async
+      );
+      arrowFunctionExpression.returnType = node.returnType;
+      arrowFunctionExpression.typeParameters = node.typeParameters;
+
+      const declarator = t.variableDeclarator(
+        identifier,
+        arrowFunctionExpression
+      );
+
+      path.replaceWith(t.variableDeclaration("const", [declarator]));
+
+      path.stop();
+    })
+  );
+}
+
+function createVisitor(
+  selection: Selection,
+  onMatch: (path: t.NodePath<t.FunctionDeclaration>) => void
+): t.Visitor {
+  return {
+    FunctionDeclaration(path) {
+      if (!selection.isInsidePath(path)) return;
+
+      onMatch(path);
+    }
+  };
+}
