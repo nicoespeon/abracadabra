cbb2ddfcd31aeaf282b65f9a87a0e7567d06f8f0
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-08-20 22:03:41 -0400

    Expose new types

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,39 @@ import { Selection } from "./editor/selection";
 import { Visitor, NodePath } from "./ast";
 
 export {
+  Refactoring,
+  RefactoringWithActionProvider,
+  Operation,
   DeprecatedRefactoring,
   DeprecatedRefactoringWithActionProvider,
   DeprecatedOperation
 };
+
+interface Refactoring {
+  command: {
+    key: string;
+    operation: Operation;
+  };
+}
+
+interface RefactoringWithActionProvider {
+  command: {
+    key: string;
+    title: string;
+    operation: Operation;
+  };
+  actionProvider: {
+    message: string;
+    isPreferred?: boolean;
+    createVisitor: (
+      selection: Selection,
+      onMatch: (path: NodePath) => void
+    ) => Visitor;
+    updateMessage?: (path: NodePath) => string;
+  };
+}
+
+type Operation = (editor: Editor) => Promise<void>;
 
 interface DeprecatedRefactoring {
   command: {
