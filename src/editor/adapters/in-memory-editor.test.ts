0c4de987b81c1f1d09a73fdbaca6b28334d8326e
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-08-20 21:22:50 -0400

    Move describe() to editor test file
    So we can add more custom tests

--- src/editor/adapters/in-memory-editor.test.ts
+++ src/editor/adapters/in-memory-editor.test.ts
@@ -3,7 +3,9 @@ import { Position } from "../position";
 
 import { InMemoryEditor } from "./in-memory-editor";
 
-createEditorContractTests("InMemory", (code, position = new Position(0, 0)) => {
-  const editor = new InMemoryEditor(code, position);
-  return [editor, () => ({ code: editor.code, position: editor.position })];
+describe("InMemory Editor", () => {
+  createEditorContractTests((code, position = new Position(0, 0)) => {
+    const editor = new InMemoryEditor(code, position);
+    return [editor, () => ({ code: editor.code, position: editor.position })];
+  });
 });
