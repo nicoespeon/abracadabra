114bca4b151af52e009a30f9daf6fb24d8755e2f
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-09-05 11:44:42 -0400

    Make VueVSCodeEditor extend VSCodeEditor

--- src/editor/adapters/create-vscode-editor.ts
+++ src/editor/adapters/create-vscode-editor.ts
@@ -1,12 +1,11 @@
 import * as vscode from "vscode";
 
-import { Editor } from "../editor";
 import { VSCodeEditor } from "./vscode-editor";
 import { VueVSCodeEditor } from "./vue-vscode-editor";
 
 export { createVSCodeEditor };
 
-function createVSCodeEditor(): Editor | undefined {
+function createVSCodeEditor(): VSCodeEditor | undefined {
   const activeTextEditor = vscode.window.activeTextEditor;
   if (!activeTextEditor) return;
 
--- src/editor/adapters/vue-vscode-editor.ts
+++ src/editor/adapters/vue-vscode-editor.ts
@@ -1,176 +1,49 @@
-import * as vscode from "vscode";
-
-import {
-  Editor,
-  Code,
-  Modification,
-  Command,
-  ErrorReason,
-  errorReasonToString,
-  Choice
-} from "../editor";
+import { Code } from "../editor";
 import { Selection } from "../selection";
-import { Position } from "../position";
+import { VSCodeEditor } from "./vscode-editor";
 
 export { VueVSCodeEditor };
 
-class VueVSCodeEditor implements Editor {
-  private editor: vscode.TextEditor;
-  private document: vscode.TextDocument;
-
-  constructor(editor: vscode.TextEditor) {
-    this.editor = editor;
-    this.document = editor.document;
-  }
-
+class VueVSCodeEditor extends VSCodeEditor {
   get code(): Code {
-    return this.fullCode.slice(
+    return super.code.slice(
       this.openingScriptTagOffset,
       this.closingScriptTagOffset
     );
   }
 
   get selection(): Selection {
-    const offsetCodeLines = this.fullCode
+    const offsetCodeLines = super.code
       .slice(0, this.openingScriptTagOffset)
       .split("\n");
     const offsetLinesCount = offsetCodeLines.length - 1;
 
-    // TODO: replace with super.selection if we subclass VSCodeEditor
-    const vsCodeSelection = createSelectionFromVSCode(this.editor.selection);
-
     return Selection.fromPositions(
-      vsCodeSelection.start.removeLines(offsetLinesCount),
-      vsCodeSelection.end.removeLines(offsetLinesCount)
+      super.selection.start.removeLines(offsetLinesCount),
+      super.selection.end.removeLines(offsetLinesCount)
     );
   }
 
   private get openingScriptTagOffset(): Offset {
-    return this.fullCode.indexOf("<script>") + "<script>".length;
+    return super.code.indexOf("<script>") + "<script>".length;
   }
 
   private get closingScriptTagOffset(): Offset {
-    return this.fullCode.indexOf("</script>");
+    return super.code.indexOf("</script>");
   }
 
-  private get fullCode(): Code {
-    // TODO: replace with super.code if we subclass VSCodeEditor
-    return this.document.getText();
-  }
+  // TODO: offset selection accordingly
+  // async write(code: Code, newCursorPosition?: Position): Promise<void>
 
-  async write(code: Code, newCursorPosition?: Position): Promise<void> {
-    // TODO: replace code in script tags when we write
-    // We need to register initial position BEFORE we update the document.
-    const cursorAtInitialStartPosition = new vscode.Selection(
-      this.editor.selection.start,
-      this.editor.selection.start
-    );
+  // TODO: replace code in script tags when we write
+  // async readThenWrite(
+  //   selection: Selection,
+  //   getModifications: (code: Code) => Modification[],
+  //   newCursorPosition?: Position
+  // ): Promise<void>
 
-    const edit = new vscode.WorkspaceEdit();
-    const allDocumentRange = new vscode.Range(
-      new vscode.Position(0, 0),
-      new vscode.Position(this.document.lineCount, 0)
-    );
-
-    edit.set(this.document.uri, [new vscode.TextEdit(allDocumentRange, code)]);
-
-    await vscode.workspace.applyEdit(edit);
-
-    // Put cursor at correct position
-    this.editor.selection = newCursorPosition
-      ? toVSCodeCursor(newCursorPosition)
-      : cursorAtInitialStartPosition;
-
-    // Scroll to correct position if it changed
-    if (newCursorPosition) {
-      const position = toVSCodePosition(newCursorPosition);
-      this.editor.revealRange(
-        new vscode.Range(position, position),
-        vscode.TextEditorRevealType.Default
-      );
-    }
-  }
-
-  async readThenWrite(
-    selection: Selection,
-    getModifications: (code: Code) => Modification[],
-    newCursorPosition?: Position
-  ): Promise<void> {
-    // TODO: replace code in script tags when we write
-    const startPosition = toVSCodePosition(selection.start);
-    const endPosition = toVSCodePosition(selection.end);
-
-    const readCode = this.document.getText(
-      new vscode.Range(startPosition, endPosition)
-    );
-
-    const textEdits = getModifications(readCode).map(({ code, selection }) => {
-      const startPosition = toVSCodePosition(selection.start);
-      const endPosition = toVSCodePosition(selection.end);
-
-      return new vscode.TextEdit(
-        new vscode.Range(startPosition, endPosition),
-        code
-      );
-    });
-
-    const edit = new vscode.WorkspaceEdit();
-    edit.set(this.document.uri, textEdits);
-
-    await vscode.workspace.applyEdit(edit);
-
-    if (newCursorPosition) {
-      this.editor.selection = toVSCodeCursor(newCursorPosition);
-    }
-  }
-
-  async delegate(command: Command) {
-    await vscode.commands.executeCommand(toVSCodeCommand(command));
-  }
-
-  async showError(reason: ErrorReason) {
-    await vscode.window.showErrorMessage(errorReasonToString(reason));
-  }
-
-  async askUser<T>(choices: Choice<T>[]) {
-    return await vscode.window.showQuickPick(choices);
-  }
-
-  moveCursorTo(position: Position) {
-    // TODO: offset selection accordingly
-    this.editor.selection = toVSCodeCursor(position);
-    return Promise.resolve();
-  }
+  // TODO: offset selection accordingly
+  // moveCursorTo(position: Position)
 }
 
 type Offset = number;
-
-function createSelectionFromVSCode(
-  selection: vscode.Selection | vscode.Range
-): Selection {
-  return new Selection(
-    [selection.start.line, selection.start.character],
-    [selection.end.line, selection.end.character]
-  );
-}
-
-function toVSCodeCursor(position: Position): vscode.Selection {
-  return new vscode.Selection(
-    toVSCodePosition(position),
-    toVSCodePosition(position)
-  );
-}
-
-function toVSCodePosition(position: Position): vscode.Position {
-  return new vscode.Position(position.line, position.character);
-}
-
-function toVSCodeCommand(command: Command): string {
-  switch (command) {
-    case Command.RenameSymbol:
-      return "editor.action.rename";
-
-    default:
-      return "";
-  }
-}
