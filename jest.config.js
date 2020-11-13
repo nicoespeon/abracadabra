cac6d258d116bda20e13c076138b946779b93084
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-11-13 16:43:15 -0500

    Fix jest warning

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "ts-jest": {
       // With TS 3.9+ it is recommended to make TS config explicit
       // Without it, tests fail for type errors that don't exist at compilation
-      tsConfig: "tsconfig.json"
+      tsconfig: "tsconfig.json"
     }
   }
 };
