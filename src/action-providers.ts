64ec3f5813270d78f051557d7686f78637f48053
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-09-30 23:48:39 -0400

    Change function signature

--- src/action-providers.ts
+++ src/action-providers.ts
@@ -3,7 +3,7 @@ import minimatch from "minimatch";
 
 import { RefactoringWithActionProvider } from "./types";
 import * as t from "./ast";
-import { Selection } from "./editor/selection";
+import { Editor } from "./editor/editor";
 import { createVSCodeEditor } from "./editor/adapters/create-vscode-editor";
 import {
   getIgnoredFolders,
@@ -31,10 +31,8 @@ class RefactoringActionProvider implements vscode.CodeActionProvider {
     if (!editor) return NO_ACTION;
 
     try {
-      const ast = t.parse(editor.code);
-
-      return this.findApplicableRefactorings(ast, editor.selection).map(
-        (refactoring) => this.buildCodeActionFor(refactoring)
+      return this.findApplicableRefactorings(editor).map((refactoring) =>
+        this.buildCodeActionFor(refactoring)
       );
     } catch {
       // Silently fail, we don't care why it failed (e.g. code can't be parsed).
@@ -53,17 +51,17 @@ class RefactoringActionProvider implements vscode.CodeActionProvider {
     return isFolderIgnored || isPatternIgnored;
   }
 
-  private findApplicableRefactorings(
-    ast: t.File,
-    selection: Selection
-  ): Refactoring[] {
+  private findApplicableRefactorings({
+    code,
+    selection
+  }: Editor): Refactoring[] {
     const applicableRefactorings = new Map<string, Refactoring>();
 
     const refactoringsToCheck = this.refactorings.filter(
       ({ command: { key } }) => shouldShowInQuickFix(key)
     );
 
-    t.traverseAST(ast, {
+    t.traverseAST(t.parse(code), {
       enter: (path) => {
         /**
          * Hint for perf improvement
