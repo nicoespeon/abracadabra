a586984185072f4e5487dd790c5efe98dae64f50
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2021-10-14 09:29:59 -0400

    Simplify calculation

--- src/refactorings/extract/extract-variable/occurrence.ts
+++ src/refactorings/extract/extract-variable/occurrence.ts
@@ -93,20 +93,10 @@ class Occurrence<T extends t.Node = t.Node> {
     };
   }
 
-  cursorOnIdentifier(extractedOccurrences: Occurrence[]): Position {
-    const offset = extractedOccurrences
-      .map(({ modification }) => modification)
-      .filter(({ selection }) => selection.isOneLine)
-      .filter(({ selection }) =>
-        selection.startsBefore(this.modification.selection)
-      )
-      .filter(
-        ({ selection }) => !selection.isEqualTo(this.modification.selection)
-      )
-      .map(({ code, selection }) => selection.width - code.length)
-      .reduce((a, b) => a + b, 0);
-
-    return this.positionOnExtractedId.removeCharacters(offset);
+  cursorOnIdentifier(_extractedOccurrences: Occurrence[]): Position {
+    return this.positionOnExtractedId.putAtSameCharacter(
+      this.modification.selection.start
+    );
   }
 
   protected get positionOnExtractedId(): Position {
