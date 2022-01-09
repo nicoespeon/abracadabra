0c9fb4512c846132d142df7ec97f902374ee2333
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2022-01-09 13:28:29 -0500

    Revert changes on playground files
    These are intentional. Such code is designed to be dirty, so you can
    refactor it with the extension when debugging.

--- src/playground/gilded-rose.ts
+++ src/playground/gilded-rose.ts
@@ -20,11 +20,11 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name !== "Aged Brie" &&
-        this.items[i].name !== "Backstage passes to a TAFKAL80ETC concert"
+        this.items[i].name != "Aged Brie" &&
+        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
@@ -32,7 +32,7 @@ export class GildedRose {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (
-            this.items[i].name === "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
           ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
@@ -47,16 +47,16 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name !== "Aged Brie") {
+        if (this.items[i].name != "Aged Brie") {
           if (
-            this.items[i].name !== "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
           ) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name !== "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
