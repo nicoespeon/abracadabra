38c9f77aac97f0220f77417ca0be6f785a5a1f4e
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2022-02-02 09:29:45 -0500

    Use babel-jest instead of ts-jest
    It's at least 12 times faster, according to my initial tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  preset: "ts-jest",
   setupFilesAfterEnv: ["./src/test/custom-matchers.ts"],
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/out/", ".contract.test.ts"],
-  globals: {
-    "ts-jest": {
-      // With TS 3.9+ it is recommended to make TS config explicit
-      // Without it, tests fail for type errors that don't exist at compilation
-      tsconfig: "tsconfig.json"
-    }
-  }
+  testPathIgnorePatterns: ["/node_modules/", "/out/", ".contract.test.ts"]
 };
