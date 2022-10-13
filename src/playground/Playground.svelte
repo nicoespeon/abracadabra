76f65126f1348110e8e59f58d51455a7d1af1503
Simon Holmes <srsholmes@gmail.com>
2022-10-13 10:03:36 +0100

    @all-contributors please add @srsholmes for adding svelte support

--- src/playground/Playground.svelte
+++ src/playground/Playground.svelte
@@ -1,20 +1,6 @@
 <script lang="ts">
-  const add = (a: number, b: number) => a + b;
-
-  add(1, 2);
-
   const hello = 'hello';
-
   const bob = hello + ' bob';
-
-  function GroupList({ groups, onGroupClick, onGroupStart }) {}
-function useCurrentUser() {
-  return { currentUser: {} };
-}
-function useBrowser() {
-  return { isSafari: false };
-}
-  
 </script>
 
 <div>Hello World</div>
