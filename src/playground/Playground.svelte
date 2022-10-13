955aba62ffc5246b4c6e326edcc7015b8f3d1e18
Simon Holmes <srsholmes@gmail.com>
2022-10-13 10:13:15 +0100

    @all-contributors please add @srsholmes for code

--- src/playground/Playground.svelte
+++ src/playground/Playground.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   const hello = 'hello';
   const bob = hello + ' bob';
+  console.log(bob);
+
+
+  const myFunc = () => 'hello';
+  const called = myFunc();
+  console.log(called);
 </script>
 
 <div>Hello World</div>
