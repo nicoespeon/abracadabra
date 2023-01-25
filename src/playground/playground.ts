1530aced7de69547a0501fac4ea2cd95b41b9014
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2023-01-25 07:51:42 -0500

    Update default playground

--- src/playground/playground.ts
+++ src/playground/playground.ts
@@ -28,20 +28,18 @@ async function createApp({ port }: { port: number } = { port: 3306 }) {
   });
 
   app.get("/prices", async (req, res) => {
-    const { type, date, age } = req.query;
-
     const result = (
       await connection.query(
         "SELECT cost FROM `base_price` " + "WHERE `type` = ? ",
-        [type]
+        [req.query.type]
       )
     )[0][0];
 
     let cost;
-    if (age < 6) {
+    if (req.query.age < 6) {
       cost = 0;
     } else {
-      if (type !== "night") {
+      if (req.query.type !== "night") {
         const holidays = (
           await connection.query("SELECT * FROM `holidays`")
         )[0];
@@ -50,8 +48,8 @@ async function createApp({ port }: { port: number } = { port: 3306 }) {
         let reduction = 0;
         for (let row of holidays) {
           let holiday = row.holiday;
-          if (date) {
-            let d = new Date(date);
+          if (req.query.date) {
+            let d = new Date(req.query.date);
             if (
               d.getFullYear() === holiday.getFullYear() &&
               d.getMonth() === holiday.getMonth() &&
@@ -62,18 +60,18 @@ async function createApp({ port }: { port: number } = { port: 3306 }) {
           }
         }
 
-        if (!isHoliday && new Date(date).getDay() === 1) {
+        if (!isHoliday && new Date(req.query.date).getDay() === 1) {
           reduction = 35;
         }
 
         // TODO apply reduction for others
-        if (age < 15) {
+        if (req.query.age < 15) {
           cost = Math.ceil(result.cost * 0.7);
         } else {
-          if (age === undefined) {
+          if (req.query.age === undefined) {
             cost = Math.ceil(result.cost * (1 - reduction / 100));
           } else {
-            if (age > 64) {
+            if (req.query.age > 64) {
               cost = Math.ceil(result.cost * 0.75 * (1 - reduction / 100));
             } else {
               cost = Math.ceil(result.cost * (1 - reduction / 100));
@@ -81,8 +79,8 @@ async function createApp({ port }: { port: number } = { port: 3306 }) {
           }
         }
       } else {
-        if (age >= 6) {
-          if (age > 64) {
+        if (req.query.age >= 6) {
+          if (req.query.age > 64) {
             cost = Math.ceil(result.cost * 0.4);
           } else {
             cost = result.cost;
