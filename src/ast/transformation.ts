9c63cef340b6402e6a3af4bfee5fd91155e48c78
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2024-08-16 22:01:48 -0400

    Move options in a function

--- src/ast/transformation.ts
+++ src/ast/transformation.ts
@@ -11,52 +11,6 @@ export const traversePath = traverse;
 export { Binding, NodePath, Scope } from "@babel/traverse";
 export type { Visitor };
 
-export const BABEL_PARSER_OPTIONS: ParserOptions = {
-  sourceType: "module",
-  allowImportExportEverywhere: true,
-  allowReturnOutsideFunction: true,
-  startLine: 1,
-
-  // Tokens are necessary for Recast to do its magic ✨
-  tokens: true,
-
-  plugins: [
-    "asyncGenerators",
-    "bigInt",
-    "classPrivateMethods",
-    "classPrivateProperties",
-    "classProperties",
-    // Not compatible with "decorators-legacy"
-    // "decorators",
-    "decorators-legacy",
-    "doExpressions",
-    "dynamicImport",
-    // Make tests fail, not sure why
-    // "estree",
-    "exportDefaultFrom",
-    "exportNamespaceFrom",
-    // Not compatible with "typescript"
-    // "flow",
-    // "flowComments",
-    "functionBind",
-    "functionSent",
-    "importMeta",
-    "jsx",
-    "logicalAssignment",
-    "nullishCoalescingOperator",
-    "numericSeparator",
-    "objectRestSpread",
-    "optionalCatchBinding",
-    "optionalChaining",
-    "partialApplication",
-    ["pipelineOperator", { proposal: "minimal" }],
-    "placeholders",
-    "throwExpressions",
-    "topLevelAwait",
-    "typescript"
-  ]
-};
-
 export function transform(code: Code, options: TraverseOptions): Transformed {
   return transformAST(parse(code), options);
 }
@@ -132,7 +86,7 @@ export function parse(code: Code): AST {
   try {
     return recast.parse(code, {
       parser: {
-        parse: (source: Code) => babelParse(source, BABEL_PARSER_OPTIONS)
+        parse: (source: Code) => babelParse(source, getBabelParseOptions())
       },
       // VS Code considers tabs to be of size 1
       tabWidth: 1
@@ -143,6 +97,54 @@ export function parse(code: Code): AST {
       `I can't build the AST from the source code. This may be due to a syntax error that you can fix. Here's what went wrong: ${message}`
     );
   }
+}
+
+function getBabelParseOptions(): ParserOptions {
+  return {
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+    allowReturnOutsideFunction: true,
+    startLine: 1,
+
+    // Tokens are necessary for Recast to do its magic ✨
+    tokens: true,
+
+    plugins: [
+      "asyncGenerators",
+      "bigInt",
+      "classPrivateMethods",
+      "classPrivateProperties",
+      "classProperties",
+      // Not compatible with "decorators-legacy"
+      // "decorators",
+      "decorators-legacy",
+      "doExpressions",
+      "dynamicImport",
+      // Make tests fail, not sure why
+      // "estree",
+      "exportDefaultFrom",
+      "exportNamespaceFrom",
+      // Not compatible with "typescript"
+      // "flow",
+      // "flowComments",
+      "functionBind",
+      "functionSent",
+      "importMeta",
+      "jsx",
+      "logicalAssignment",
+      "nullishCoalescingOperator",
+      "numericSeparator",
+      "objectRestSpread",
+      "optionalCatchBinding",
+      "optionalChaining",
+      "partialApplication",
+      ["pipelineOperator", { proposal: "minimal" }],
+      "placeholders",
+      "throwExpressions",
+      "topLevelAwait",
+      "typescript"
+    ]
+  };
 }
 
 export function traverseAST(ast: AST, opts: TraverseOptions): AST {
