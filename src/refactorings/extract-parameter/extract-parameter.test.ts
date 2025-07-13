685ba3edcad8430cd779bf30df3ef814efdefa56
pinage404 <pinage404@gmail.com>
2025-07-13 18:16:33 +0200

    refactor group tests

--- src/refactorings/extract-parameter/extract-parameter.test.ts
+++ src/refactorings/extract-parameter/extract-parameter.test.ts
@@ -1,18 +1,21 @@
+import { describe } from "node:test";
 import { InMemoryEditor } from "../../editor/adapters/in-memory-editor";
 import { Code } from "../../editor/editor";
 import { Selection } from "../../editor/selection";
 import { extractParameter } from "./extract-parameter";
 
 describe("Extract Parameter", () => {
-  it("should convert a const to a parameter with default value", () => {
-    shouldExtractParameter({
-      code: `function sayHello() {
+  describe("should convert a const to a parameter with default value", () => {
+    it("named function", () => {
+      shouldExtractParameter({
+        code: `function sayHello() {
   const name[cursor] = "World";
   const lastName = "Doe";
 }`,
-      expected: `function sayHello(name = "World") {
+        expected: `function sayHello(name = "World") {
   const lastName = "Doe";
 }`
+      });
     });
   });
 
