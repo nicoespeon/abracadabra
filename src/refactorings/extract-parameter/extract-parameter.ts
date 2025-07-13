a6d07a668a020441ca3e312d505edc9ebe4c6a91
pinage404 <pinage404@gmail.com>
2025-07-13 19:32:57 +0200

    explain TS error

--- src/refactorings/extract-parameter/extract-parameter.ts
+++ src/refactorings/extract-parameter/extract-parameter.ts
@@ -18,7 +18,9 @@ function updateCode(ast: t.AST, selection: Selection): t.Transformed {
     createVisitor(selection, (path, functionPath) => {
       const variableDeclaration = path.node;
       const assignmentPattern = t.assignmentPattern(
-        // @ts-expect-error - Let's fix this type issue!
+        // VariableDeclarator[id] includes AssignmentPattern, RestElement and TSParameterProperty which are not included in AssignmentPattern[left]
+        // AssignmentPattern[left] includes a subset of VariableDeclarator[id]
+        // @ts-expect-error - i don't know how to remove this TS error
         variableDeclaration.id,
         variableDeclaration.init
       );
