22b7420239a53da9bb1af05fa2a2a44f61c1375a
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2025-10-16 22:08:25 -0400

    Migrate remove-jsx-fragment

--- src/refactorings/remove-jsx-fragment/remove-jsx-fragment.test.ts
+++ src/refactorings/remove-jsx-fragment/remove-jsx-fragment.test.ts
@@ -1,19 +1,18 @@
 import { InMemoryEditor } from "../../editor/adapters/in-memory-editor";
-import { Code, ErrorReason } from "../../editor/editor";
-import { testEach } from "../../tests-helpers";
+import { Code } from "../../editor/editor";
 import { removeJsxFragment } from "./remove-jsx-fragment";
 
 describe("Remove JSX Fragment", () => {
-  testEach<{ code: Code; expected: Code }>(
-    "should remove JSX fragment",
-    [
-      {
-        description: "a single div inside JSX",
+  describe("should remove JSX fragment", () => {
+    it("a single div inside JSX", () => {
+      shouldRemoveJsxFragment({
         code: "return <><div>[cursor]I'm a mere div</div></>;",
         expected: "return <div>I'm a mere div</div>;"
-      },
-      {
-        description: "a div with multiple children inside JSX",
+      });
+    });
+
+    it("a div with multiple children inside JSX", () => {
+      shouldRemoveJsxFragment({
         code: `return (<>
   <div>
     <h2>[cursor]I'm a heading</h2>
@@ -26,9 +25,11 @@ describe("Remove JSX Fragment", () => {
     <p>I'm just a paragraph</p>
   </div>
 );`
-      },
-      {
-        description: "the closest unnecessary JSX Fragment",
+      });
+    });
+
+    it("the closest unnecessary JSX Fragment", () => {
+      shouldRemoveJsxFragment({
         code: `return (<>
   <div>
     <h2>I'm a heading</h2>
@@ -43,37 +44,48 @@ describe("Remove JSX Fragment", () => {
     </div>
   </>
 );`
-      }
-    ],
-    async ({ code, expected }) => {
-      const editor = new InMemoryEditor(code);
-
-      await removeJsxFragment(editor);
-
-      expect(editor.code).toBe(expected);
-    }
-  );
-
-  it("should not remove JSX Fragment if it has 2+ children", async () => {
-    const editor = new InMemoryEditor(
-      `return <><div>Something</div>[cursor]<div>Something else</div></>`
-    );
-    const originalCode = editor.code;
-
-    await removeJsxFragment(editor);
-
-    expect(editor.code).toBe(originalCode);
+      });
+    });
   });
 
-  it("should show an error message if refactoring can't be made", async () => {
+  it("should not remove JSX Fragment if it has 2+ children", () => {
+    const code = `return <><div>Something</div>[cursor]<div>Something else</div></>`;
+    const editor = new InMemoryEditor(code);
+    const result = removeJsxFragment({
+      state: "new",
+      code: editor.code,
+      selection: editor.selection
+    });
+
+    expect(result.action).toBe("show error");
+  });
+
+  it("should show an error message if refactoring can't be made", () => {
     const code = `// This is a comment, can't be refactored`;
     const editor = new InMemoryEditor(code);
-    jest.spyOn(editor, "showError");
+    const result = removeJsxFragment({
+      state: "new",
+      code: editor.code,
+      selection: editor.selection
+    });
 
-    await removeJsxFragment(editor);
-
-    expect(editor.showError).toHaveBeenCalledWith(
-      ErrorReason.DidNotRemoveJsxFragment
-    );
+    expect(result.action).toBe("show error");
   });
 });
+
+function shouldRemoveJsxFragment({
+  code,
+  expected
+}: {
+  code: Code;
+  expected: Code;
+}) {
+  const editor = new InMemoryEditor(code);
+  const result = removeJsxFragment({
+    state: "new",
+    code: editor.code,
+    selection: editor.selection
+  });
+
+  expect(result).toMatchObject({ action: "write", code: expected });
+}
